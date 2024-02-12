4b8a762f1c569e36dadc4692c55dfa1f266927b4
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-02-12 13:51:40 -0300

    Modificado a classe Program, criada a classeStartup, também as classes Categoria e Lanche com seus atributos.Declara uma paretial view _Carousel, e adicionado 3 imagens ao projeto

---
+++ LanchesMac/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LanchesMac;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        services.AddMemoryCache();
+        services.AddSession();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseSession();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
+

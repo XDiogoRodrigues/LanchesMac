c7a3332da52ec9c4679391bacce25e293cb4d68e
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-02-14 13:35:39 -0300

    Declarado o controller do model Lanche e criado uma view para exibir todos os lanches

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMac.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var listaDeLanches = _lancheRepository.Lanches;
+            return View(listaDeLanches);
+        }
+    }
+}

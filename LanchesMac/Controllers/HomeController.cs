e3f30d899d04a8cc0406257ff7c439894b6912fc
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-02-22 11:47:50 -0300

    Declarada a view model HomeViewModel que exibir os lanches preferidos na tela inicial, adicionando algumas imagens ao projeto

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using LanchesMac.Models;
+using LanchesMac.Repository.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,24 +8,18 @@ namespace LanchesMac.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILancheRepository _lancheRepository;
+        public HomeController(ILancheRepository lancheRepository)
         {
-            _logger = logger;
+            _lancheRepository = lancheRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            HomeViewModel homeViewModel = new HomeViewModel();
+            homeViewModel.LanchesPreferidos = _lancheRepository.LanchesPreferidos;
+            return View(homeViewModel);
+        }     
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ LanchesMac/ViewModels/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.ViewModels;
+
+public class HomeViewModel
+{
+    public IEnumerable<Lanche> LanchesPreferidos { get; set; }
+}

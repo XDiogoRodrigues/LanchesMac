afc595d7b994411efe3571db7f774fc7626200c8
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-03-04 18:29:43 -0300

    Declarado o Metodo Ckeckout post no controller Pedido e sua view

--- LanchesMac/Controllers/PedidoController.cs
+++ LanchesMac/Controllers/PedidoController.cs
@@ -25,6 +25,47 @@ public class PedidoController : Controller
     [HttpPost]
     public IActionResult Checkout(Pedido pedido)
     {
-        return View();
+        int totalItensPedido = 0;
+        decimal PrecoTotalPedido = 0.0m;
+
+        //obtem os itens do carrinho de compra do cliente
+        List<ItemDoCarrinho> itens = _carrinhoCompra.GetCarrinhoCompraItens();
+        _carrinhoCompra.CarrinhoCompraItems = itens;
+
+        //verificar se existem itens de pedido
+        if(_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+        {
+            ModelState.AddModelError("", "Seu carrinho está vazio, que tal incluir um lanche...");
+        }
+
+        //calcula o total de itens e o total do pedido
+        foreach(var item in itens)
+        {
+            totalItensPedido += item.Quantidade;
+            PrecoTotalPedido += item.Lanche.Preco * item.Quantidade;
+        }
+
+        //atribui os valores obtidos ao pedido
+        pedido.TotalItensPedido = totalItensPedido;
+        pedido.PedidoTotal = PrecoTotalPedido;
+
+        //valida os dados do pedido
+        if (ModelState.IsValid)
+        {
+            //cria o pedido e os detalhes
+            _pedidoRepository.CriarPedido(pedido);
+
+            //define mensagens ao cliente
+            ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido";
+            ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+            //limpa o carrinho do cliente
+            _carrinhoCompra.LimparCarrinho();
+
+            //exibe a view com dados do cliente e do pedido
+            return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+        }
+
+        return View(pedido);
     }
 }

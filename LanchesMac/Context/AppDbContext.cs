575d49641ed243730efcf05389f15b29b4fda85d
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-03-04 15:20:04 -0300

    Populadas as tabelas Pedidos e PedidoDetalhes. Declarada as classes Pedido e PedidoDetalhe como Models, Criado a Interface IPedidoRepository e a Classe PedidoRepository

--- LanchesMac/Context/AppDbContext.cs
+++ LanchesMac/Context/AppDbContext.cs
@@ -12,5 +12,7 @@ namespace LanchesMac.Context
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set;}
         public DbSet<ItemDoCarrinho> ItensDoCarrinho { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
     }
 }
